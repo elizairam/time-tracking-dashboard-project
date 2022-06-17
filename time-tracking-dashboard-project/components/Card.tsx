21e68790e3b0ce4c3fa249952927149dc3ef9c34
elizabete silva <elizabete.airam@gmail.com>
2022-06-16 22:14:30 -0300

    feat: add components

---
+++ time-tracking-dashboard-project/components/Card.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+interface ICard {
+  icon: ReactNode;
+  title: string;
+  current: number;
+  previous: number;
+  dataReference: string;
+}
+
+export default function Card(props: ICard) {
+  return (
+    <>
+      <h5>{props.title}</h5>
+      <h1>{props.current}</h1>
+      <h6>
+        {"Last"} {props.dataReference} {"-"}{props.previous} { "hrs"}
+      </h6>
+    </>
+  );
+}
